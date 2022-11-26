ba50731c9a4d1e040740f5691dba449b879fc403
Pari <apple@Apples-MacBook-Pro.local>
2022-11-26 11:44:21 +0530

    Initial commit

---
+++ app/src/main/java/com/example/hackernews/ui/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.hackernews.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.hackernews.R
+import dagger.hilt.android.AndroidEntryPoint
+
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
